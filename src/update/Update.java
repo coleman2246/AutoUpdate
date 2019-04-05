4175cad8518a059ac6d9e06e5e1794f496b2d3b4
Cole <coleman2246@github.com>
2019-04-05 01:23:53 -0400

    No supports version detection and copy and paste of files

--- src/update/Update.java
+++ src/update/Update.java
@@ -6,8 +6,15 @@
 package update;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.channels.FileChannel;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+
 
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
@@ -22,8 +29,34 @@ import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
  */
 
 public class Update {
+	
+	
+	
+	public static void copyFile(String fileSource, String fileDest) throws IOException {
+        //copying and pasting folder 
+        FileChannel sourceChannel = null;
+        FileChannel destChannel = null;
 
-	public static Git returnGit(String dir, String os) throws GitAPIException, IOException {
+        File modsFolderGit = new File(fileSource);
+        File modsFolderLocal = new File(fileDest);
+        
+       
+        	
+        
+        sourceChannel = new FileInputStream(modsFolderGit).getChannel();
+        
+        destChannel = new FileOutputStream(modsFolderLocal).getChannel();
+
+     
+        destChannel.transferFrom(sourceChannel, 0, sourceChannel.size());
+
+        sourceChannel.close();
+        destChannel.close();
+        }
+	
+	
+	
+	public static Git returnGit(String dir, String os,String user) throws GitAPIException, IOException {
 		Git git = null;
 		/*
 		FileRepositoryBuilder builder = new FileRepositoryBuilder();
@@ -35,7 +68,7 @@ public class Update {
 				.build();
 				*/
 		//git = new Git(repo);
-		dir = dir + "/UOIT-Craft-Modpack";
+		dir = dir + "\\UOIT-Craft-Modpack";
 
 		File gitRepo = new File(dir);
 
@@ -50,9 +83,19 @@ public class Update {
 		} catch (Exception e) {
             try {
                 git = new Git(localRepo);
-                git.pull().call();
+                String parseResult = git.pull().call().toString();
+                
+               
+                if("up-to-date.".equals(parseResult.substring(parseResult.length()-12,parseResult.length()-1))) {
+                	System.out.println("No update Avaliable");
+                	return git;
+                }else {
+                	System.out.println("Update Downloading");
+                }
+       
             } catch (Exception f) {
                 System.out.println("\n UPDATE FAILED: report on https://github.com/coleman2246/UOIT-Craft-Modpack.git");
+    
             }
 
 		}
@@ -71,13 +114,19 @@ public class Update {
 		// getting os
 		String os = System.getProperty("os.name").toLowerCase();
 		String userName = System.getProperty("user.name");
-
+		System.out.println(userName);
+		boolean testing = true;
+		
+		
+		
 		String repoDir = "";
 		if (os.contains("windows")) {
 			// git = returnGit(repoDir);
-			repoDir = "";
+			// C:\\Users\\cole2\\Twitch\\Minecraft\\Instances\\UOIT CRACT\\
+			repoDir = "C:\\Users\\"+userName+"\\Twitch\\Minecraft\\Instances\\UOIT CRACT";
 
-		} else if (os.contains("linux")) {
+		} else if (os.contains("linux")&& testing) {
+		
 			repoDir = "/home/mitchell/Documents/temp/updateMine/";
 		} else if (os.contains("mac")) {
             // TODO find dir for mac
@@ -87,7 +136,7 @@ public class Update {
 			System.exit(1);
 		}
 
-		Git git = returnGit(repoDir, os);
+		Git git = returnGit(repoDir, os,userName);
 
 	}
 
